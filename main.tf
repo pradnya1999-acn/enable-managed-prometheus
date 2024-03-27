9e95ec816d2e3694fc0b34faf4a1fb55d241287d
pradnya1999-acn <159138875+pradnya1999-acn@users.noreply.github.com>
2024-03-27 19:17:12 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,24 @@
+provider "google" {
+#   version = "= 3.55.0"
+  credentials = var.sa_key
+  project = "parabolic-base-409505"
+}
+ 
+variable "sa_key" {
+  default =  ""
+}
+
+resource "google_container_cluster" "primary" {
+  name     = "my-gke-cluster"
+  location = "us-central1"
+  initial_node_count       = 1
+  # enable_autopilot   = true
+
+    monitoring_config {
+    enable_components = ["SYSTEM_COMPONENTS"]
+    managed_prometheus {
+      enabled = true
+    }
+  }
+
+}
