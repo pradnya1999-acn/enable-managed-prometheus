3188a06fb2ca9a61dbe995178ec88692e1bc21ec
pradnya1999-acn <159138875+pradnya1999-acn@users.noreply.github.com>
2024-04-26 14:48:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,8 @@ resource "google_container_cluster" "primary" {
   name     = "my-gke-cluster"
   location = "us-central1"
   initial_node_count       = 1
-  enable_autopilot   = true
+ # enable_autopilot   = true
+  networking_mode = "ROUTES"
 
     monitoring_config {
     enable_components = ["SYSTEM_COMPONENTS"]
